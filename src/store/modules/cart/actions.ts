53c68018108cd17bddf202b9523369d6e91de3e3
thefalked <giulianocrivelli@hotmail.com>
2021-07-08 00:09:28 -0400

    splitting product actions

--- src/store/modules/cart/actions.ts
+++ src/store/modules/cart/actions.ts
@@ -1,10 +1,28 @@
 import { IItemProduct } from "./types";
 
-export function addProductToCart(product: IItemProduct) {
+export function addProductToCartRequest(product: IItemProduct) {
   return {
-    type: "ADD_PRODUCT_TO_CART",
+    type: "ADD_PRODUCT_TO_CART_REQUEST",
     payload: {
       product,
+    },
+  };
+}
+
+export function addProductToCartSuccess(product: IItemProduct) {
+  return {
+    type: "ADD_PRODUCT_TO_CART_SUCCESS",
+    payload: {
+      product,
+    },
+  };
+}
+
+export function addProductToCartFailure(productId: number) {
+  return {
+    type: "ADD_PRODUCT_TO_CART_FAILURE",
+    payload: {
+      productId,
     },
   };
 }
