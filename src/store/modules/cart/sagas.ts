7eed2845864c75bfe4889f015cbd540211332186
thefalked <giulianocrivelli@hotmail.com>
2021-07-08 00:20:38 -0400

    finishing stock check

--- src/store/modules/cart/sagas.ts
+++ src/store/modules/cart/sagas.ts
@@ -1,9 +1,22 @@
-import { all, select, takeLatest } from "redux-saga/effects";
+import { AxiosResponse } from "axios";
+import { all, call, put, select, takeLatest } from "redux-saga/effects";
+
+import api from "../../../services/api";
+
 import { IState } from "../..";
 
-import { addProductToCartRequest } from "./actions";
+import {
+  addProductToCartFailure,
+  addProductToCartRequest,
+  addProductToCartSuccess,
+} from "./actions";
 
 type CheckProductStockRequest = ReturnType<typeof addProductToCartRequest>;
+
+interface IStockResponse {
+  id: number;
+  quantity: number;
+}
 
 function* checkProductStock({ payload }: CheckProductStockRequest) {
   const { product } = payload;
@@ -15,7 +28,16 @@ function* checkProductStock({ payload }: CheckProductStockRequest) {
     );
   });
 
-  console.log(currentQuantity);
+  const availableStockResponse: AxiosResponse<IStockResponse> = yield call(
+    api.get,
+    `stock/${product.id}`
+  );
+
+  if (availableStockResponse.data.quantity > currentQuantity) {
+    yield put(addProductToCartSuccess(product));
+  } else {
+    yield put(addProductToCartFailure(product.id));
+  }
 }
 
 export default all([
