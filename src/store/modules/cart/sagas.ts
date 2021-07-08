762ef9a26ee039b96d03b18a3b35bd4650b1efa4
thefalked <giulianocrivelli@hotmail.com>
2021-07-08 00:01:04 -0400

    verifying product quantity on cart

--- src/store/modules/cart/sagas.ts
+++ src/store/modules/cart/sagas.ts
@@ -1,7 +1,21 @@
-import { all, takeLatest } from "redux-saga/effects";
+import { all, select, takeLatest } from "redux-saga/effects";
+import { IState } from "../..";
 
-function checkProductStock() {
-  console.log("adding to cart");
+import { addProductToCart } from "./actions";
+
+type CheckProductStockRequest = ReturnType<typeof addProductToCart>;
+
+function* checkProductStock({ payload }: CheckProductStockRequest) {
+  const { product } = payload;
+
+  const currentQuantity: number = yield select((state: IState) => {
+    return (
+      state.cart.items.find((item) => item.product.id === product.id)
+        ?.quantity ?? 0
+    );
+  });
+
+  console.log(currentQuantity);
 }
 
 export default all([takeLatest("ADD_PRODUCT_TO_CART", checkProductStock)]);
